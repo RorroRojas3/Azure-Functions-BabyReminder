9f7bfa1392b53a0a9b69855b190eebe9a94fb69d
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2020-11-22 16:53:15 -0500

    Sending emails correctly with image

--- Services/Implementation/SendEmailService.cs
+++ Services/Implementation/SendEmailService.cs
@@ -24,15 +24,32 @@ namespace net_core_baby_reminder.Services.Implementation
 
                 MailMessage email = new MailMessage();
                 SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-
-                email.From = new MailAddress("rorro.irg@gmail.com");
-                email.To.Add("rorro.irg@gmail.com");
+                var currentDate = DateTime.UtcNow;
+                var deliveryDate = new DateTime(2021, 2, 5);
+                var days = (deliveryDate.Date - currentDate.Date).Days;
+                var body = @"<html>
+                                <body>
+                                    <h1>Faltan " + days + " dias para conocer al bebecito</h1>" +
+                                    "<img src=cid:myImageID alt='BEBECITO' width=\"500\" height=\"600\"/>" +
+                                "</body>" +
+                            "</html>";
+                AlternateView htmlView = AlternateView.CreateAlternateViewFromString(body, null, "text/html");
+                LinkedResource theEmailImage = new LinkedResource("./Images/DaniOLeo.jpeg")
+                {
+                    ContentId = "myImageID"
+                };
+                htmlView.LinkedResources.Add(theEmailImage);
+                
+                email.From = new MailAddress("rodrigo3.tech@gmail.com");
+                email.AlternateViews.Add(htmlView);
+                email.To.Add("rorro.irg@gmail.com,Macarena.crg@hotmail.com,Francisco2.ins@gmail.com,Ingrid.pgc@hotmail.com,carlos.rojas@fluor.com");
                 email.Subject = "Test Mail";
-                email.Body = "This is for testing SMTP mail from GMAIL";
+                email.IsBodyHtml = true;
+                email.Body = body;
 
                 SmtpServer.Port = 587;
                 SmtpServer.UseDefaultCredentials = false;
-                SmtpServer.Credentials = new NetworkCredential("rorro.irg@gmail.com", "AndroidApple2019!");
+                SmtpServer.Credentials = new NetworkCredential("rodrigo3.tech@gmail.com", "OpenSource2019!");
                 SmtpServer.EnableSsl = true;
 
 
