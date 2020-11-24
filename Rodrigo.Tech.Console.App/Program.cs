1c15a9e8e7e7adb6be8e8adf742f9a73254b890c
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2020-11-23 20:03:41 -0500

    Console application base done to call Azure Durable function

--- Rodrigo.Tech.Console.App/Program.cs
+++ Rodrigo.Tech.Console.App/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Rodrigo.Tech.Services.Implementation;
+using Rodrigo.Tech.Services.Interface;
+using System;
+using System.Threading.Tasks;
 
-namespace Rodrigo.Tech.Console.App
+namespace Rodrigo.Tech.ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static async Task Main()
         {
-            
+            var serviceProvider = new ServiceCollection()
+            .AddLogging()
+            .AddTransient<IHttpClientService, HttpClientService>()
+            .BuildServiceProvider();
+
+            var httpClientService = serviceProvider.GetService<IHttpClientService>();
+            var url = "https://www.google.com";
+            var response = await httpClientService.Json<object>(url, HttpMethods.Get);
+            var responseContent = await response.Content.ReadAsStringAsync();
         }
     }
 }
