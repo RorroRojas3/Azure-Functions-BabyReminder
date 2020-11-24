0f38979b98c811135c0a1e123d61809e9ef0d039
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2020-11-23 20:08:32 -0500

    Console application done

--- Rodrigo.Tech.Console.App/Program.cs
+++ Rodrigo.Tech.Console.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Rodrigo.Tech.Services.Implementation;
 using Rodrigo.Tech.Services.Interface;
 using System;
@@ -16,10 +17,27 @@ namespace Rodrigo.Tech.ConsoleApp
             .AddTransient<IHttpClientService, HttpClientService>()
             .BuildServiceProvider();
 
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+            .CreateLogger<Program>();
+            logger.LogInformation($"ConsoleApp - Baby reminder - Started");
+
             var httpClientService = serviceProvider.GetService<IHttpClientService>();
             var url = "https://www.google.com";
+
+            logger.LogInformation($"ConsoleApp - Baby reminder - Calling {url}");
             var response = await httpClientService.Json<object>(url, HttpMethods.Get);
+
+            logger.LogInformation($"ConsoleApp - Baby reminder - Reading response content");
             var responseContent = await response.Content.ReadAsStringAsync();
+            logger.LogInformation($"ConsoleApp - Baby reminder - Response: {responseContent}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                logger.LogError($"ConsoleApp - BabyReminder - Unsuccesfull call to {url}, " +
+                    $"Status: {response.StatusCode}, Response: {responseContent}");
+            }
+
+            logger.LogInformation($"ConsoleApp - Baby reminder - Finished");
         }
     }
 }
